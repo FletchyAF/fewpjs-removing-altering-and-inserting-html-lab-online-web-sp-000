e0a8cc0d8cc9a06a662faa1fce5da67bd7a92c3a
Joshua Fletcher <joshua.f1998@gmail.com>
2021-05-31 20:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("main#main").remove();
-
-let newHeader = document.createElement("h1")
-newHeader.id = "victory"
-newHeader.innerHTML = "Joshua is the champion!";
-newHeader.className = "victory";
